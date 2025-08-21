2d8bcd27b01eeaae60d8d57a7423bfa609098bd3
nguyenquyen1998 <quyennv@vnpay.vn>
2025-08-21 16:21:14 +0700

    add: api + config web api

---
+++ src/Monolith.Core.WebAPI/Program.cs
@@ -0,0 +1,17 @@
+using Monolith.Core.Infrastructure;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCoreInfrastructure(builder.Configuration);
+
+var app = builder.Build();
+
+app.UseHttpsRedirection();
+
+
+
+
+
+app.Run();
+
+
