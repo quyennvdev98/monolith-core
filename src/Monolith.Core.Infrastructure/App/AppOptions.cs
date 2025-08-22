f821e3d10271ee625ae92c252300c6cee07950ef
nguyenquyen1998 <quyennv@vnpay.vn>
2025-08-22 15:17:29 +0700

    update: AppOptions

--- src/Monolith.Core.Infrastructure/App/AppOptions.cs
+++ src/Monolith.Core.Infrastructure/App/AppOptions.cs
@@ -2,7 +2,7 @@ namespace Monolith.Core.Infrastructure.App;
 
 public class AppOptions
 {
-    public string AppName { get; set; }
+    public string AppName { get; set; } = "Core";
     public string InstanceId { get; init; } = Guid.NewGuid().ToString();
-    public string Version { get; set; }
+    public string Version { get; set; } = "1.0.0";  
 }
