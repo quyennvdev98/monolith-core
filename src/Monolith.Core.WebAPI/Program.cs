b8c6ef5531a955badb417788be0bdcee54d4b7a8
nguyenquyen1998 <quyennv@vnpay.vn>
2025-08-22 15:10:39 +0700

    update: swagger

--- src/Monolith.Core.WebAPI/Program.cs
+++ src/Monolith.Core.WebAPI/Program.cs
@@ -1,14 +1,25 @@
 using Monolith.Core.Infrastructure;
+using Monolith.Core.Infrastructure.Swagger;
+using Newtonsoft.Json;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddControllers()
+        .AddNewtonsoftJson(opts => opts.SerializerSettings.DateTimeZoneHandling = DateTimeZoneHandling.Utc);
+
 builder.Services.AddCoreInfrastructure(builder.Configuration);
+
+builder.Services.AddSwagger(builder.Configuration); 
 
 var app = builder.Build();
 
-app.UseHttpsRedirection();
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwaggerDocs();
+}
 
-
+app.MapControllers();
 app.Run();
 
 
